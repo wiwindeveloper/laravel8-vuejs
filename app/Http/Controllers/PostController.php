0f8630c084ad64177c896c6e505a33133c7a0b1b
CoopWorldIndonesia <n.wiwinsavitri@gmail.com>
2022-11-08 16:22:36 +0800

    restful api

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //get data from table posts
+        $posts = Post::latest()->get();
+
+        //make response JSON
+        return response()->json([
+            'success' => true,
+            'message' => 'List Data Post',
+            'data' => $posts
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        //find post by id
+        $post = Post::findOrfail($id);
+
+        //make response json
+        return response()->json([
+            'success' => true,
+            'message' => 'Detail Data Post',
+            'data' => $posts
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        //set validation
+        $vaidator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        //response error validation
+        if($validator->fails())
+        {
+            return response()->json($validator-errors(), 400);
+        }
+
+        //save to database
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        // success post save to database
+        if($post){
+            return response()->json([
+                'success' => true,
+                'message' => 'Post Created',
+                'data' => $post
+            ], 201);
+        }
+
+        //failed save to database
+        return response()->json([
+            'success' => false,
+            'message' => 'Post failed to save',
+            'data' => $post
+        ], 409);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        //set validation
+        $vaidator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        //response error validation
+        if($validator->fails())
+        {
+            return response()->json($validator-errors(), 400);
+        }
+
+        //find post by id
+        $post = Post::findOrFail($post->id);
+
+        if($post)
+        {
+            //update post 
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Post update',
+                'data' => $post
+            ], 200);
+        }
+
+        //data post not found
+        return response()->json([
+            'success' => false,
+            'message' => 'Post Not Found',
+        ], 404);
+    }
+
+    public function destroy($id)
+    {
+        //find post by ID
+        $post = Post::findOrFail($id);
+
+        if($post)
+        {
+            //delete post
+            $post->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Post Deleted',
+            ], 200);
+        }
+
+        //data post not found
+        return response()->json([
+            'success' => false,
+            'message' => 'Post Not Found',
+        ], 404);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ *  Route Resource post
+ * */ 
+Route::resource('/post', PostController::class);
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
